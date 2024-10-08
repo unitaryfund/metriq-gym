dfff77487545f9e85c9d8caf99a846d42ce8db9a
Daniel Strano <stranoj@gmail.com>
2024-10-08 15:53:08 -0400

    Update parse.py

--- metriq_gym/parse.py
+++ metriq_gym/parse.py
@@ -9,15 +9,12 @@ def parse_arguments() -> argparse.Namespace:
         Parsed arguments as an argparse.Namespace object.
     """
     parser = argparse.ArgumentParser(description="Quantum volume certification")
-    parser.add_argument("-n", type=int, default=8, help="Number of qubits")
-    parser.add_argument("-s", "--shots", type=int, default=8, help="Number of shots (default is 8)")
-    parser.add_argument("-t", "--trials", type=int, default=8, help="Number of trials to run (default is 8)")
-    parser.add_argument("-b", "--backend", type=str, default="qasm_simulator", help="Backend to use")
-    parser.add_argument("--token", type=str, help="IBM Quantum API token")
+    parser.add_argument("-n", type=int, default=8, help="Number of qubits (default is 8)")
+    parser.add_argument("-s", "--shots", type=int, default=8, help="Number of shots per trial (default is 8)")
+    parser.add_argument("-t", "--trials", type=int, default=8, help="Number of trials (default is 8)")
+    parser.add_argument("-b", "--backend", type=str, default="qasm_simulator", help="Backend to use (default is \"qasm_simulator\"")
+    parser.add_argument("--token", type=str, help="IBM Quantum API token (must be supplied to run on real hardware)")
 
     args = parser.parse_args()
-
-    if args.shots is None:
-        args.shots = 1 << args.n  # Default to 2^n shots
 
     return args
