97b32fd498853354e08047faa3effed298632f14
Alessandro Cosentino <alessandro@unitary.fund>
2025-03-13 12:30:20 +0100

    QML Kernel benchmark single-trial implementation (#265)

--- metriq_gym/benchmarks/__init__.py
+++ metriq_gym/benchmarks/__init__.py
@@ -1,4 +1,5 @@
 from metriq_gym.benchmarks.benchmark import Benchmark, BenchmarkData
+from metriq_gym.benchmarks.qml_kernel import QMLKernel, QMLKernelData
 from metriq_gym.benchmarks.quantum_volume import QuantumVolume, QuantumVolumeData
 from metriq_gym.benchmarks.bseq import BSEQ, BSEQData
 from metriq_gym.job_type import JobType
@@ -6,14 +7,17 @@ from metriq_gym.job_type import JobType
 BENCHMARK_HANDLERS: dict[JobType, type[Benchmark]] = {
     JobType.QUANTUM_VOLUME: QuantumVolume,
     JobType.BSEQ: BSEQ,
+    JobType.QML_KERNEL: QMLKernel,
 }
 
 BENCHMARK_DATA_CLASSES: dict[JobType, type[BenchmarkData]] = {
     JobType.QUANTUM_VOLUME: QuantumVolumeData,
     JobType.BSEQ: BSEQData,
+    JobType.QML_KERNEL: QMLKernelData,
 }
 
 SCHEMA_MAPPING = {
     JobType.QUANTUM_VOLUME: "quantum_volume.schema.json",
     JobType.BSEQ: "bseq.schema.json",
+    JobType.QML_KERNEL: "qml_kernel.schema.json",
 }
